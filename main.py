5298220f2b411ba532d8ec6125115d319ce2cd5e
Matthew Delgado <mdelgado2k@gmail.com>
2022-10-17 16:10:21 -0400

    instructions have been read

---
+++ main.py
@@ -0,0 +1,18 @@
+# allows use of turtles
+import turtle
+# set window size
+turtle.setup(400,600)
+
+# draw frame for traffic light
+def drawLightFrame():
+
+
+# t - turtle handle
+# ht - number of pixels to move forward circle location
+# color - circle fill color
+def drawCircle(t,ht,color):
+
+
+# state machine for traffic light, controls infinite state machine, creation and execution of program
+def trafficState_machine():
+
