d60718d70d69dfc7791a07b123f17d0d6a409ac9
Matthew Delgado <mdelgado2k@gmail.com>
2022-10-24 20:00:50 -0400

    end of assignment

--- main.py
+++ main.py
@@ -4,29 +4,29 @@ import turtle
 # set window size
 turtle.setup(400,600)
 
-# Create a playground for turtles
-wn = turtle.Screen()
-wn.bgcolor('green')
+# Create window
+window = turtle.Screen()
+window.bgcolor('green')
 
-# Create turtles
-tess = turtle.Turtle()
-alex = turtle.Turtle()
-henry = turtle.Turtle()
+# name your tmn turtles
+donatello = turtle.Turtle()
+rafael = turtle.Turtle()
+michaelangelo = turtle.Turtle()
 
 # draw frame for traffic light
 def drawLightFrame():
-    tess.pensize(3)
-    tess.color('black', 'white')
-    tess.begin_fill()
-    tess.forward(80)
-    tess.left(90)
-    tess.forward(200)
-    tess.left(90)
-    tess.forward(80)
-    tess.left(90)
-    tess.forward(200)
-    tess.left(90)
-    tess.end_fill()
+    donatello.pensize(3)
+    donatello.color('black', 'black')
+    donatello.begin_fill()
+    donatello.forward(50)
+    donatello.left(90)
+    donatello.forward(120)
+    donatello.left(90)
+    donatello.forward(50)
+    donatello.left(90)
+    donatello.forward(120)
+    donatello.left(90)
+    donatello.end_fill()
 
 drawLightFrame()
 
@@ -35,44 +35,44 @@ drawLightFrame()
 # color - circle fill color
 def circle(t, ht, color):
     t.penup()
-    t.forward(40)
+    t.forward(25)
     t.left(90)
     t.forward(ht)
     t.shape('circle')
     t.fillcolor(color)
 
-circle(tess, 40, 'green')
-circle(alex, 100, 'orange')
-circle(henry, 160, 'red')
+circle(donatello, 30, 'green')
+circle(rafael, 60, 'orange')
+circle(michaelangelo, 90, 'red')
 
-# This variable holds the current state of the machine
+# current state of machine
 state = 0
 
-# A state machine for traffic light. Controls the infinite state machine, the creation and execution of the program
+# Controls infinite state machine
 def trafficState_machine():
     global state 
 
     if state == 0: 
-        henry.color('darkgrey')
-        alex.color('darkgrey')
-        tess.color('green')
-        wn.ontimer(trafficState_machine, 3000)  
+        michaelangelo.color('darkgrey')
+        rafael.color('darkgrey')
+        donatello.color('green')
+        window.ontimer(trafficState_machine, 5000)  
         state = 1
     elif state == 1:  
-        henry.color('darkgrey')
-        alex.color('orange')
-        tess.color('darkgrey')
-        wn.ontimer(trafficState_machine, 2000)
+        michaelangelo.color('darkgrey')
+        rafael.color('orange')
+        donatello.color('darkgrey')
+        window.ontimer(trafficState_machine, 5000)
         state = 2
     elif state == 2:               
-        henry.color('red')
-        alex.color('darkgrey')
-        wn.ontimer(trafficState_machine, 2000)
+        michaelangelo.color('red')
+        rafael.color('darkgrey')
+        window.ontimer(trafficState_machine, 5000)
         state = 0
 
 trafficState_machine()
 
 # listen for events
-wn.listen()
+window.listen()
 # Wait for user to close window
-wn.mainloop()  
+window.mainloop()  
